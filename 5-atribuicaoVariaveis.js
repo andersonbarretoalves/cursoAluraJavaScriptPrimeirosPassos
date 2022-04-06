6e3ef35d3f37e3428908a4e30af917f3f4030fe8
Anderson Barreto Alves <anderson.alves@grupoimne.local>
2022-04-06 17:50:01 -0300

    Array

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,5 +1,5 @@
 console.log("trabalhando com Atribuição de Variáveis");
-const idade = 41;
+
 const primeiroNome = "Anderson";
 const sobrenome = "Alves";
 
@@ -7,5 +7,14 @@ const sobrenome = "Alves";
 console.log(primeiroNome, sobrenome);
 console.log(`Meu nome é: ${primeiroNome} ${sobrenome}`);
 
+let contador = 0;
+contador = contador + 1;
+
 const nomeCompleto = primeiroNome + sobrenome;
-console.log(nomeCompleto)
+console.log(nomeCompleto);
+
+let idade; //Declarando variavel
+idade = 41; //atribuindo valor
+idade = idade + 1;
+console.log(idade);
+
---
+++ 6-listas.js
@@ -0,0 +1,15 @@
+console.log(`trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`); //Adicionando um item na lista
+console.log("Destinos Possíveis: ");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
